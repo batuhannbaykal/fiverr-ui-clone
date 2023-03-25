9eb418e29522652691a4772a503c1b57af673a75
batuhannbaykal <baykalbatuhan98@gmail.com>
2023-03-25 16:15:01 +0300

    fiverr ui clone project

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,110 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="top">
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Garhics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyles</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Garhics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyles</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Garhics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyles</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Garhics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyles</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+          <div className="item">
+            <h2>Categories</h2>
+            <span>Garhics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Translation</span>
+            <span>Video & Animation</span>
+            <span>Music & Audio</span>
+            <span>Programming & Tech</span>
+            <span>Data</span>
+            <span>Business</span>
+            <span>Lifestyles</span>
+            <span>Photography</span>
+            <span>Sitemap</span>
+          </div>
+        </div>
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <h2>fiverr</h2>
+            <span>Fiverr International Ltd. 2023</span>
+          </div>
+          <div className="right">
+            <div className="social">
+              <img src={require("../../img/twitter.png")} alt="" />
+              <img src={require("../../img/facebook.png")} alt="" />
+              <img src={require("../../img/linkedin.png")} alt="" />
+              <img src={require("../../img/pinterest.png")} alt="" />
+              <img src={require("../../img/instagram.png")} alt="" />
+            </div>
+            <div className="link">
+              <img src={require("../../img/language.png")} alt="" />
+              <span>English</span>
+            </div>
+            <div className="link">
+              <img src={require("../../img/coin.png")} alt="" />
+              <span>USD</span>
+            </div>
+            <img src={require("../../img/accessibility.png")} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
